57df8b2c3a69485baba618ceb7a51cba177f9b7d
Nicolas Takashi <nicolas.tcs@hotmail.com>
2015-01-19 13:49:15 -0200

    Adicionando LSP

---
+++ Ntakashi/Ntakashi.LSP/Retangulo.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Ntakashi.LSP
+{
+    public class Retangulo
+    {
+
+        protected int Largura;
+        protected int Altura;
+        public int Area;
+
+        public virtual void InserirLargura(int largura)
+        {
+            Largura = largura;
+        }
+
+        public virtual void InserirAltura(int altura)
+        {
+            Altura = altura;
+        }
+
+        public int ObterLargura()
+        {
+            return Largura;
+        }
+
+        public int ObterAltura()
+        {
+            return Altura;
+        }
+
+    }
+}
