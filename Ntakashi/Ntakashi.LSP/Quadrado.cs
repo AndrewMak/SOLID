e8a4471ef7eefd3c256d65fe4dd3a091bb77a9a9
Nicolas Takashi <nicolas.tcs@hotmail.com>
2015-01-19 13:44:28 -0200

    Adicionando LSP

---
+++ Ntakashi/Ntakashi.LSP/Quadrado.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Ntakashi.LSP
+{
+    public class Quadrado : Retangulo
+    {
+        public override void InserirLargura(int largura)
+        {
+            base.Largura = largura;
+            base.Altura = largura;
+        }
+
+        public override void InserirAltura(int altura)
+        {
+            base.Altura = altura;
+            base.Largura = altura;
+        }
+    }
+}
